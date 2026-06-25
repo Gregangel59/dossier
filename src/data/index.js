59f7deb7896cd326a2f9d194ecef9db4eecddda2
Gregangel59 <gregory.pluquin@gmail.com>
2026-06-25 10:47:18 +0200

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -12,12 +12,14 @@
 import americanExpress from "./amex.js";
 import spacex from "./spacex.js";
 import intuitivesurgical from "./isrg.js";
+import airliquide from "./air-liquide.js";
 
 // L'ordre du tableau = l'ordre d'affichage sur la page d'accueil.
 export const COMPANIES = [
   americanExpress,
   spacex,
   intuitivesurgical,
+  airliquide
     // amd,
   // adyen,
   // toast,
